4954008b48adb2adf813a35f59925229070d6789
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2026-02-15 15:24:17 +0000

    WIP Upgrade 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_02_15_120330) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_15_151615) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
