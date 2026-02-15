ed202856240fe20a751bd173470478fd630f5779
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2026-02-15 15:49:08 +0000

    Add deterministic exncryption on email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  encrypts :email, deterministic: true
+
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
 end
